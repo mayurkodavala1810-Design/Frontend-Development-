c59be61d70b8e24baed3d700c2fe985b91965ada
MayurKodavala <mayurkodavala1810@gmail.com>
2026-08-15 05:30:44 +0000

    This a array in loop

---
+++ 05_iteration_js/04_Array_Loop.js
@@ -0,0 +1,29 @@
+const myObj = {
+    Name: "Mayur",
+    Age: 25,
+    City: "Ahmedabad"
+}
+for(const key in myObj) {
+    // console.log(`${key}:- ${myObj[key]}`);
+}
+
+const Proggraming = {
+    Name: "JavaScript",
+    Type: "Scripting Language",
+    Year: 1995,
+    Creator: "Brendan Eich"
+}
+for (const key in Proggraming) {
+    // console.log(Proggraming[key]);
+}
+
+// const map = new Map()
+// map.set("Name", "Mayur")
+// map.set("Age", 25)
+// map.set("City", "Ahmedabad")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+
