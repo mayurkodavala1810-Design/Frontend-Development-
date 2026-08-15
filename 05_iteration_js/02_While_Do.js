e2b5713a09488496082e47543d0ce7e453c1d025
MayurKodavala <mayurkodavala1810@gmail.com>
2026-08-15 05:27:39 +0000

    This is a While and do Loop

---
+++ 05_iteration_js/02_While_Do.js
@@ -0,0 +1,20 @@
+let i = 1
+while (i <= 10) {
+    // console.log(`Value of index is ${i}`);
+    // i = i + 2
+    i = i + 1
+}
+
+let myarr = ["Mayur", "Anuj", "Keyur", "Raj", "Rumit"]
+let arr = 0
+while(arr < myarr.length) {
+    console.log(`Value is ${myarr[arr]}`);
+    arr++
+}
+
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
